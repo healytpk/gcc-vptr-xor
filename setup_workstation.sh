2edb9872fc74f72144adc02b8823e73b54d01920
Thomas PK Healy <thomaspkhealy@yahoo.com>
2025-11-21 11:38:47 +0000

    Shell script to set up workstation to build

---
+++ setup_workstation.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+export CONFIG_SHELL=/bin/sh
+echo ===================================================================
+echo   Installing pre-requisites
+echo ===================================================================
+sudo apt update
+sudo apt install -y build-essential gcc-multilib git make gawk flex bison libgmp-dev libmpfr-dev libmpc-dev python3 binutils perl libisl-dev libzstd-dev tar gzip bzip2 autotools-dev automake
+sudo apt install -y meld geany
+./contrib/download_prerequisites
+mkdir -p ../gcc-thomas-healy_build
+cd ../gcc-thomas-healy_build
+if [ -z "$(find . -maxdepth 1 -type f -print -quit)" ]; then
+  echo ===================================================================
+  echo   Creating build directory and configuring build:
+  echo      configure --prefix=`realpath --no-symlinks "$PWD/../gcc-thomas-healy"`/ --enable-languages=c,c++ --with-multilib-list=m32,m64,mx32
+  echo ===================================================================
+  ../gcc-thomas-healy_source/configure --prefix=`realpath --no-symlinks "$PWD/../gcc-thomas-healy"`/ --enable-languages=c,c++ --with-multilib-list=m32,m64,mx32
+fi
+echo ===================================================================
+echo   Configuring git at the commandline with username and password
+echo ===================================================================
+git config --global user.email "thomaspkhealy@yahoo.com"
+git config --global user.name "Thomas PK Healy"
+echo ===================================================================
+echo   Setting up bikeshed
+echo ===================================================================
+sudo apt-get install -y python3 python3-venv pipx
+pipx ensurepath
+. "$HOME/.profile"
+pipx install bikeshed
+bikeshed update
+echo ===================================================================
+echo   ALL DONE
+echo ===================================================================
