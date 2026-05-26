b0e9141cd7fad2c266de8f6960c1bd46a923ec0c
Thomas PK Healy <thomaspkhealy@yahoo.com>
2026-05-26 11:07:46 +0100

    update to setup_workstation_mswin

--- setup_workstation_mswin.sh
+++ setup_workstation_mswin.sh
@@ -1,35 +1,70 @@
 #!/bin/sh
-export SUFFIX=_mswin
-export TARGET=x86_64-w64-mingw32
+CONFIG_SHELL=/bin/sh
+export CONFIG_SHELL
 
-export CONFIG_SHELL=/bin/sh
+SUFFIX=_mswin
 
-export PREFIX=`realpath --no-symlinks "$PWD/../gcc-thomas-healy${SUFFIX}"`
-export SYSROOT="/usr/${TARGET}/"
+configure_target() {
+  TARGET="$1"
 
-# System headers will be looked for in $SYSROOT/mingw/include instead
-# of $SYSROOT/include, therefore we must create a symbolic link
-if [ ! -e /usr/${TARGET}/mingw ]; then
-  echo "Creating symbolic link from /usr/$TARGET/mingw to /usr/$TARGET/"
-  sudo ln -sf /usr/${TARGET}/ /usr/${TARGET}/mingw
-fi
+  PREFIX="$(realpath --no-symlinks "$PWD/../gcc-thomas-healy${SUFFIX}$2/")"
+  SYSROOT="/usr/${TARGET}/"
+  BUILD_DIR="../gcc-thomas-healy${SUFFIX}$2_build/"
 
-# Must create symbolic links to tools like the assembler and linker
-mkdir -p ../gcc-thomas-healy${SUFFIX}/${TARGET}
-if [ ! -e ../gcc-thomas-healy${SUFFIX}/${TARGET}/bin ]; then
-  echo "Creating symbolic link from ../gcc-thomas-healy${SUFFIX}/${TARGET}/bin to /usr/${TARGET}/bin/"
-  ln -sf /usr/${TARGET}/bin/ ../gcc-thomas-healy${SUFFIX}/${TARGET}/bin
-fi
+  echo "==================================================================="
+  echo "Configuring target: $TARGET"
+  echo "  PREFIX    : $PREFIX"
+  echo "  SYSROOT   : $SYSROOT"
+  echo "  BUILD_DIR : $BUILD_DIR"
+  echo "==================================================================="
 
-mkdir -p ../gcc-thomas-healy${SUFFIX}_build
-cd ../gcc-thomas-healy${SUFFIX}_build
-if [ -z "$(find . -maxdepth 1 -type f -print -quit)" ]; then
-  echo ===================================================================
-  echo   Creating build directory and configuring build:
-  echo      configure --prefix=`realpath --no-symlinks "$PWD/../gcc-thomas-healy${SUFFIX}"`/ --disable-multilib --enable-languages=c,c++ --prefix="$PREFIX" --target="$TARGET" --with-sysroot="$SYSROOT" --with-build-sysroot="$SYSROOT" --with-native-system-header-dir=/include/
-  echo ===================================================================
-  ../gcc-thomas-healy_source/configure --prefix=`realpath --no-symlinks "$PWD/../gcc-thomas-healy${SUFFIX}"`/ --disable-multilib --enable-languages=c,c++ --prefix="$PREFIX" --target="$TARGET" --with-sysroot="$SYSROOT" --with-build-sysroot="$SYSROOT"  --with-native-system-header-dir=/include/
-fi
-echo ===================================================================
-echo   ALL DONE
-echo ===================================================================
+  # System headers will be looked for in $SYSROOT/mingw/include instead
+  # of $SYSROOT/include, therefore create a symbolic link if needed.
+  if [ ! -e "/usr/${TARGET}/mingw" ]; then
+    echo "Creating symbolic link from /usr/${TARGET}/mingw to /usr/${TARGET}/"
+    sudo ln -sf "/usr/${TARGET}/" "/usr/${TARGET}/mingw"
+  fi
+
+  # Must create symbolic links to tools like the assembler and linker.
+  mkdir -p "$PREFIX/$TARGET"
+  if [ ! -e "$PREFIX/$TARGET/bin" ]; then
+    echo "Creating symbolic link from $PREFIX/$TARGET/bin to /usr/${TARGET}/bin/"
+    sudo ln -sf "/usr/${TARGET}/bin/" "$PREFIX/$TARGET/bin"
+  fi
+
+  cd "$BUILD_DIR"
+
+  if [ -z "$(find . -maxdepth 1 -type f -name Makefile -print -quit)" ]; then
+    echo "==================================================================="
+    echo "Creating build directory and configuring build for $TARGET:"
+    echo "  ../gcc-thomas-healy_source/configure \\"
+    echo "    --prefix=${PREFIX} \\"
+    echo "    --enable-languages=c,c++ \\"
+    echo "    --disable-multilib \\"
+    echo "    --target=${TARGET} \\"
+    echo "    --with-sysroot=${SYSROOT} \\"
+    echo "    --with-build-sysroot=${SYSROOT} \\"
+    echo "    --with-native-system-header-dir=/include/"
+    echo "==================================================================="
+
+    ../gcc-thomas-healy_source/configure \
+      --prefix="$PREFIX" \
+      --enable-languages=c,c++ \
+      --disable-multilib \
+      --target="$TARGET" \
+      --with-sysroot="$SYSROOT" \
+      --with-build-sysroot="$SYSROOT" \
+      --with-native-system-header-dir=/include/
+  else
+    echo "Build directory for $TARGET already contains files; skipping configure."
+  fi
+
+  cd -
+}
+
+configure_target x86_64-w64-mingw32 64
+configure_target i686-w64-mingw32   32
+
+echo "==================================================================="
+echo "ALL DONE"
+echo "==================================================================="
